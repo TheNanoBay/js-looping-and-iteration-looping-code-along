23421483155ad15e91a169331e62d3f900ad93c5
TheNanoBay <noorani.mehdi@outlook.com>
2021-02-01 20:44:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(stringNames, eventName){
+    for(let i = 0; i < stringNames.length; i++){
+        return stringNames.map(x => `Thank you, ${x}, for the wonderful surprise gift!`);
+    }
+}
+
+function countDown(positiveInteger) {
+    while(positiveInteger >= 0){
+        console.log(positiveInteger);
+        positiveInteger --; 
+    }
+}
+
